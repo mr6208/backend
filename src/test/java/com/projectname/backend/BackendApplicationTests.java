00949b27d9b268120022d16e06a8273b52c5fab4
정우혁 <alalalalalalall367@gmail.com>
2024-08-13 18:28:15 +0900

    chore : 개발환경 세팅

---
+++ src/test/java/com/projectname/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectname.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
